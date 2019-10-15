14b3c368b4632062752d07625c2b3fc63b3093a5
Edunno <tamericandream@hotmail.com>
2019-10-15 11:41:08 +0200

    Draw and make empty mazes
    Fiunction for drawing mazes  with spaced 1's and 0's added.
    Function for

--- src/controller.py
+++ src/controller.py
@@ -1,11 +1,23 @@
+
+def makeRoomsInMaze(mArray):
+    for x in range(len(mArray)):
+        if x > 0 and x < len(mArray)-1 and x%2==1:
+            for y in range(len(mArray[x])):
+                if y > 0 and y < len(mArray[x])-1 and y%2==1:
+                    mArray[x][y] = 0
+    return mArray
+
 def makeAmazingMazes(sizeX,sizeY):
-    mazeArray = [[1]*sizeX]*sizeY
-    for x in range(len(mazeArray)):
+    sizeX = sizeX*2+1
+    sizeY = sizeY*2+1
+    mazeArray = [[1 for x in range(sizeX)] for y in range(sizeY)]
+    mazeArray = makeRoomsInMaze(mazeArray)
+    for x in mazeArray:
         line = ""
-        for y in range(len(mazeArray[x])):
-            line += str(mazeArray[x][y])+" "
+        for y in x:
+            line += str(y)+" "
         print(line)
 
-    
 
-makeAmazingMazes(8,6)
+
+makeAmazingMazes(4,4)
