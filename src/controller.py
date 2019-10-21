af056969a062cf1a5d9ebb95131a49a5d387845a
Edunno <tamericandream@hotmail.com>
2019-10-21 10:41:31 +0200

    Y axis fix

--- src/controller.py
+++ src/controller.py
@@ -150,17 +150,20 @@ theTimes = list(solvingTimes.values())
 
 # Drawing stuff
 
-plt.cla()
+#plt.cla()
 fig, ax1 = plt.subplots()
+ax1.set_ylabel('Time for solve', color='tab:blue')
 # bar(x-vals, y-vals, bar width, align bar relative to x-val on x-axis) )
 ax1.bar(tryList, theTimes, width=0.5, align='center')
 ax2 = ax1.twinx()
 
 ax2.bar(tryList, pvList, width=0.3, align='edge',color='orange')
+ax2.set_ylabel('Attempts', color='tab:orange')
+ax2.tick_params(axis='y')
 # plt.ticklabel_format(useOffset=False)
-plt.axis([0, len(solvingTimes)+1, 0, max(theTimes)+0.01])  # axis(x-min, x-max, y-min, y-max)
+#plt.axis([0, len(solvingTimes)+1, 0, max(theTimes)+0.01])  # axis(x-min, x-max, y-min, y-max)
 plt.title("Barplot Time to solve", fontsize=12)
 plt.xlabel("Attempt", fontsize=10)
-plt.ylabel("TimeSolved", fontsize=10)
+#plt.ylabel("TimeSolved", fontsize=10)
 plt.tick_params(axis='both', which='major', labelsize=10)
 plt.show()
