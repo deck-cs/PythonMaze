20020c04a35419a3f1d05ce097ead77c197a1e78
Edunno <tamericandream@hotmail.com>
2019-11-21 11:18:15 +0100

    Update SubController.py

--- src/Controller/SubController.py
+++ src/Controller/SubController.py
@@ -4,20 +4,54 @@ class SubController:
     def __init__(self, amount, sizes):
         self.amount = amount
         self.sizes = sizes
-        
+        self.solvedTimesList = []
+        self.attempts = {}
+        self.tryList = []
+        self.iteration = 0
+        self.theTimes = []
+        self.relatedValues = []
+        self.pvListFinal = []
+
 
     def makeMazesWithStats(self):
         for x in range(self.sizes):
             mazes = mazeCon.MazeController(self.amount,self.sizes[x])
+            self.makeStatNumbers(mazes.solvedTimesList)
+            self.getRelatedValues(mazes.pvList)
 
 
-
-    def makeStatNumbers(self, solvingTimes, relatedValues):
-        global solvingTimes
-        global relatedValues
-        solvingTimes = {}
+    def makeStatNumbers(self, solvedTimesList):
         x = 1
-        for aTry in solvedTimesList:
-            solvingTimes.setdefault(x, 0)
-            solvingTimes[x] = aTry
+        for aTry in self.solvedTimesList:
+            self.attempts.setdefault(x, 0)
+            self.attempts[x] = aTry
             x+=1
+
+    def getStatKeys(self):
+        min = "min"
+        min+=str(self.iteration)
+        avg = "avg"
+        avg+=str(self.iteration)
+        max = "max"
+        max+= str(self.iteration)
+        self.tryList.append(min)
+        self.tryList.append(avg)
+        self.tryList.append(max)
+
+    def getStatValues(self):
+        self.theTimes.append(min(list(self.attempts.values())))
+        self.theTimes.append(sum(list(self.attempts.values()))/len(list(self.attempts.values())))
+        self.theTimes.append(max(list(self.attempts.values())))
+
+    def getRelatedValues(self, pvList):
+        solveList = list(self.attempts.values())
+        minKey = min(self.attempts, key=self.attempts.get)-1
+        self.relatedValues.append(min(solveList)/pvList[minKey])
+        self.relatedValues.append((sum(solveList)/len(solveList))/((sum(pvList)/len(pvList))))
+        maxKey = max(self.attempts, key=self.attempts.get)-1
+        self.relatedValues.append(min(solveList)/pvList[maxKey])
+
+    def getPvListFinal(self, pvList):
+        self.pvListFinal.append(min(pvList))
+        self.pvListFinal.append(sum(pvList)/len(pvList))
+        self.pvListFinal.append(max(pvList))
