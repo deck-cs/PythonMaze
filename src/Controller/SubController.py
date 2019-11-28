f727e601f52eb57e5a73384b57f493a81fc17808
Edunno <tamericandream@hotmail.com>
2019-11-28 10:02:57 +0100

    Graph stuff
    Added graph, not tested

--- src/Controller/SubController.py
+++ src/Controller/SubController.py
@@ -1,4 +1,5 @@
 from Controller import MazeController as mazeCon
+import matplotlib.pyplot as plt
 
 class SubController:
     def __init__(self, amount, sizesList):
@@ -59,3 +60,29 @@ class SubController:
         self.pvListFinal.append(min(pvList))
         self.pvListFinal.append(sum(pvList)/len(pvList))
         self.pvListFinal.append(max(pvList))
+
+    def makePlots(self):
+        fig1, ax1 = plt.subplots()
+    plt.xticks(rotation=45)
+    ax1.set_ylabel('Time for solve', color='tab:blue')
+    # bar(x-vals, y-vals, bar width, align bar relative to x-val on x-axis) )
+    ax1.bar(tryList, theTimes, width=0.5, align='center')
+    ax2 = ax1.twinx()
+    ax2.bar(tryList, pvList, width=0.3, align='edge',color='orange')
+    ax2.set_ylabel('Attempts', color='tab:orange')
+    ax2.tick_params(axis='y')
+    # plt.ticklabel_format(useOffset=False)
+    #  plt.axis([0, len(solvingTimes)+1, 0, max(theTimes)+0.01])  # axis(x-min, x-max, y-min, y-max)
+    plt.title("Barplot Time to solve and places visited", fontsize=10)
+    plt.xlabel("Attempt", fontsize=10)
+    #plt.ylabel("TimeSolved", fontsize=10)
+    plt.tick_params(axis='both', which='major', labelsize=10)
+    # Plot 2
+    fig2, ax3 = plt.subplots()
+    plt.xticks(rotation=45)
+    plt.title("Time over places visited")
+    plt.xlabel("Attempt", fontsize=10)
+    ax3.bar(tryList, relatedValues, width = 0.3, align='center')
+    ax3.set_ylabel('Time pr. move', color='tab:red')
+    ax3.tick_params(axis='y')
+    plt.show()
