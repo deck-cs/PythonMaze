6d6bfe9820f5daae0fc2348667c1dd5cea24e134
Dan Tomicic <dan.tomicic@gmail.com>
2019-12-05 12:07:21 +0100

    mazesarray in mainCon

--- src/Controller_MainController.py
+++ src/Controller_MainController.py
@@ -6,10 +6,12 @@ class MainController:
     def __init__(self, sizesList, amount):
         self.sizesList = sizesList
         self.amount = amount
+        self.mazesArray = []
 
     def runMain(self):
         c1 = sCon.SubController(self.amount, self.sizesList)
         c1.makeMazesWithStats()
+        self.mazesArray = c1.loadFromJSON()
 
 
 if __name__ == "__main__":
