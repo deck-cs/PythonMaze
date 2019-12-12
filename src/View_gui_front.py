3ac8402268ca5743b67916e17f1bf73d826f40a4
Kim P. Pedersen <kimp-mbp@Kims-MacBook-Pro.local>
2019-12-12 19:43:25 +0100

    cleanup done

--- src/View_gui_front.py
+++ src/View_gui_front.py
@@ -19,6 +19,7 @@ MAZE_SIZE_RELX = 0.1
 MAZE_SIZE_RELY = 0.15
 WIDGET_SIZE_RELWIDTH = 0.8
 WIDGET_SIZE_RELHEIGHT = 0.07
+WIDGET_LABEL_RELHEIGHT = 0.05
 # Frame sizes in %
 LEFT_FRAME_SIZE = 0.25
 RIGHT_FRAME_SIZE = 0.25
@@ -75,7 +76,6 @@ def draw_maze(selected_maze):
             # NOTE the order of y and x in the next line
             character = selected_maze[y][x]
             # Calculate the screen x, y coordinates
-            # CUBE_SIZE = 25
             CUBE_SIZE = lower_frame.winfo_width()/len(selected_maze[y])
             screen_x = (x * CUBE_SIZE)
             screen_y = (y * CUBE_SIZE)
@@ -184,12 +184,12 @@ def startMazeSolve():
 
 minMazeSize_scale = tk.Scale(left_frame, orient='horizontal', from_=5,
                              to=35, resolution=5, command=slide_valueMin)
-minMazeSize_scale.place(relx=MAZE_SIZE_RELX, rely=0.44, relwidth=WIDGET_SIZE_RELWIDTH,
+minMazeSize_scale.place(relx=MAZE_SIZE_RELX, rely=0.39, relwidth=WIDGET_SIZE_RELWIDTH,
                         relheight=WIDGET_SIZE_RELHEIGHT)
 
 maxMazeSize_scale = tk.Scale(left_frame, orient='horizontal', from_=5,
                              to=35, resolution=5, command=slide_valueMax)
-maxMazeSize_scale.place(relx=MAZE_SIZE_RELX, rely=0.60, relwidth=WIDGET_SIZE_RELWIDTH,
+maxMazeSize_scale.place(relx=MAZE_SIZE_RELX, rely=0.53, relwidth=WIDGET_SIZE_RELWIDTH,
                         relheight=WIDGET_SIZE_RELHEIGHT)
 
 ############ CHECKBOX ############
@@ -202,7 +202,7 @@ checkbox_saveData.place(relx=0.5, rely=0.5, relwidth=0.3, relheight=0.25)
 
 spinbox_SolveItterations = tk.Spinbox(left_frame, from_=1, to=40)
 spinbox_SolveItterations.place(
-    relx=MAZE_SIZE_RELX, rely=0.3, relwidth=WIDGET_SIZE_RELWIDTH, relheight=WIDGET_SIZE_RELHEIGHT)
+    relx=MAZE_SIZE_RELX, rely=0.27, relwidth=WIDGET_SIZE_RELWIDTH, relheight=0.05)
 
 ############ LISTBOX ############
 
@@ -210,32 +210,33 @@ scroolbar = tk.Scrollbar(listbox_frame)
 Lb1 = tk.Listbox(listbox_frame, yscrollcommand=scroolbar.set)
 
 ############ LABELS ############
+left_label = tk.Label(left_frame, text='Select number of itterations')
+left_label.place(relx=MAZE_SIZE_RELX, rely=0.2, relwidth=WIDGET_SIZE_RELWIDTH,
+                 relheight=WIDGET_SIZE_RELHEIGHT)
 
 left_label = tk.Label(left_frame, text='Select minimum size of maze')
-left_label.place(relx=MAZE_SIZE_RELX, rely=0.38, relwidth=WIDGET_SIZE_RELWIDTH,
-                 relheight=WIDGET_SIZE_RELHEIGHT)
+left_label.place(relx=MAZE_SIZE_RELX, rely=0.33, relwidth=WIDGET_SIZE_RELWIDTH,
+                 relheight=WIDGET_LABEL_RELHEIGHT)
 
 left_label = tk.Label(left_frame, text='Select maximum size of maze')
-left_label.place(relx=MAZE_SIZE_RELX, rely=0.54, relwidth=WIDGET_SIZE_RELWIDTH,
-                 relheight=WIDGET_SIZE_RELHEIGHT)
+left_label.place(relx=MAZE_SIZE_RELX, rely=0.47, relwidth=WIDGET_SIZE_RELWIDTH,
+                 relheight=WIDGET_LABEL_RELHEIGHT)
 
 title_label = tk.Label(top_frame, text='THE AMAZING MAZE SOLVER')
 title_label.place(relx=0.25, rely=0.1, relheight=0.25, relwidth=0.5)
 
 title_listbox_label = tk.Label(right_frame, text='List of mazes')
 title_listbox_label.place(
-    relx=0.1, rely=0.22, relheight=0.07, relwidth=WIDGET_SIZE_RELWIDTH)
+    relx=0.1, rely=0.2, relheight=0.07, relwidth=WIDGET_SIZE_RELWIDTH)
+
+bottom_label = tk.Label(bottom_frame, text='DECK-CS © 2019')
+bottom_label.place(relx=0.5, rely=0.15, anchor='n')
 
 ############ BUTTONS ############
 
 buttonStartMaze = tk.Button(top_frame, bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, text='Solve maze(s)', bd=5,
                             command=lambda: startMazeSolve())
 buttonStartMaze.place(relx=0.19, rely=0.5, relwidth=0.3, relheight=0.25)
-
-# buttonStartMaze = tk.Button(left_frame, bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, text='Set data', bd=5,
-#                             command=lambda: setMazeData())
-# buttonStartMaze.place(relx=MAZE_SIZE_RELX, rely=MAZE_SIZE_RELY,
-#                       relwidth=WIDGET_SIZE_RELWIDTH, relheight=0.1)
 
 buttonSelectMaze = tk.Button(right_frame, bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR, text='Select maze', bd=5,
                              command=mazeSelect)
