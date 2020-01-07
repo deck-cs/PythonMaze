adb98fdc21161c76901dbc0ab74aa174f700c163
Edunno <tamericandream@hotmail.com>
2020-01-07 12:55:49 +0100

    Update ModelMazeSolver.py
    lek

--- src/ModelMazeSolver.py
+++ src/ModelMazeSolver.py
@@ -31,10 +31,10 @@ class MazeSolver:
         maze[x][y] = '3'
         self.pv += 1
         # explore neighbors clockwise starting by the one on the right - This is where recursive functions use the boolean values to check which way it can move
-        if ((x < len(maze)-1 and self.search(x+1, y, maze, startTime))
-            or (y > 0 and self.search(x, y-1, maze, startTime))
-            or (x > 0 and self.search(x-1, y, maze, startTime))
-                or (y < len(maze)-1 and self.search(x, y+1, maze, startTime))):
+        if ((x < len(maze)-1 and self.search(x+1, y, maze, startTime)) #Looks down, and checks if out of bounds.
+            or (y < len(maze)-1 and self.search(x, y+1, maze, startTime)) #Looks to the right, and checks if out of bounds.
+            or (x > 0 and self.search(x, y-1, maze, startTime)) #Looks to the left
+                or (y > 0 and self.search(x-1, y, maze, startTime))): #Looks up
             return True
         maze[x][y]='4'
         return False
