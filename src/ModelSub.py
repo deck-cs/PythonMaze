36804f189c2185c6729297f036b1bf9bba3e3c14
Kim P. Pedersen <kimp-mbp@Kims-MacBook-Pro-2.local>
2020-01-08 13:34:06 +0100

    Comments

--- src/ModelSub.py
+++ src/ModelSub.py
@@ -112,10 +112,10 @@ class SubModel:
         ax2.set_ylabel('Attempts', color='tab:orange')
         ax2.tick_params(axis='y')
         # plt.ticklabel_format(useOffset=False)
-        #  plt.axis([0, len(solvingTimes)+1, 0, max(theTimes)+0.01])  # axis(x-min, x-max, y-min, y-max)
+        # plt.axis([0, len(solvingTimes)+1, 0, max(theTimes)+0.01])  # axis(x-min, x-max, y-min, y-max)
         plt.title("Barplot Time to solve and places visited", fontsize=10)
         plt.xlabel("Attempt", fontsize=10)
-        #plt.ylabel("TimeSolved", fontsize=10)
+        # plt.ylabel("TimeSolved", fontsize=10)
         plt.tick_params(axis='both', which='major', labelsize=10)
         # Plot 2 - Time over places visited
         fig2 = plt.figure()
